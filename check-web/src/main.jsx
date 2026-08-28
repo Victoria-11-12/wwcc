481e84c14f3912a304be534b14acc21e0c915b0f
Victoria <2162096926@qq.com>
2026-08-28 22:58:14 +0800

    feat: 配置antd

--- check-web/src/main.jsx
+++ check-web/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import { App } from './App.jsx'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+
+
+<ConfigProvider locale={zhCN}>
+  <App />
+</ConfigProvider>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
