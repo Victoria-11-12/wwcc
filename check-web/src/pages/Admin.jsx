66d1a0c1ea7640db4599a0c981248d72dd684e91
Victoria <2162096926@qq.com>
2026-08-30 20:26:04 +0800

    feat: 管理页UI库使用antd

--- check-web/src/pages/Admin.jsx
+++ check-web/src/pages/Admin.jsx
@@ -1,4 +1,11 @@
+import { Button } from 'antd'
+
 // 管理页（路径 /admin）
 export const Admin = () => {
-  return <h1>管理页</h1>
+  return (
+    <div>
+      <h1>管理页</h1>
+      <Button type="primary">测试 antd</Button>
+    </div>
+  )
 }
--- check-web/src/pages/Check.jsx
+++ check-web/src/pages/Check.jsx
@@ -1,11 +1,4 @@
-import { Button } from 'antd'
-
 // 查重页（路径 /check）
 export const Check = () => {
-  return (
-    <div>
-      <h1>查重页</h1>
-      <Button type="primary">测试 antd</Button>
-    </div>
-  )
+  return <h1>查重页</h1>
 }
